30050c3e73c8770f28ebdbc8bbd39c3c8a14c3a6
Your Name <you@example.com>
2025-01-03 20:03:33 +0530

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0', // Allows access from any device on the local network
-    port: 3000,      // Optional: Specify the port you want to use
-  },
+  base: '/portfolio/'
+  
 })
